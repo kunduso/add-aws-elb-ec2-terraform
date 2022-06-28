60587ad4518d5039dfaeda890820483a2cebf71e
Sourav Kundu <sourav12kundu@gmail.com>
2022-06-28 07:02:51 -0500

    added provider, backend configs and removed tfvars from commit history

---
+++ variables.tf
@@ -0,0 +1,18 @@
+#Define AWS Region
+variable "region" {
+  description = "Infrastructure region"
+  type        = string
+  default     = "us-east-2"
+}
+#Define IAM User Access Key
+variable "access_key" {
+  description = "The access_key that belongs to the IAM user"
+  type        = string
+  sensitive   = true
+}
+#Define IAM User Secret Key
+variable "secret_key" {
+  description = "The secret_key that belongs to the IAM user"
+  type        = string
+  sensitive   = true
+}
