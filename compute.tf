6183aac2a5084e5a40b7be3f99bfb90bb0dcb3e1
Sourav Kundu <sourav12kundu@gmail.com>
2022-07-08 14:56:35 -0500

    added ami filter and ec2 instances

--- compute.tf
+++ compute.tf
@@ -20,3 +20,37 @@
     "Name" = "Application-1-sg"
   }
 }
+data "aws_ami" "amazon_ami" {
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0.20220606.1-x86_64-gp2"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  most_recent = true
+  owners      = ["amazon"]
+}
+resource "aws_instance" "app-server1" {
+  instance_type          = "t2.micro"
+  ami                    = data.aws_ami.amazon_ami.id
+  vpc_security_group_ids = [aws_security_group.http-sg.id]
+  subnet_id              = aws_subnet.private-2a.id
+  associate_public_ip_address = true
+    tags = {
+      Name = "app-server-1"
+    }
+  user_data = file("user_data/user_data.tpl")
+}
+resource "aws_instance" "app-server2" {
+  instance_type          = "t2.micro"
+  ami                    = data.aws_ami.amazon_ami.id
+  vpc_security_group_ids = [aws_security_group.http-sg.id]
+  subnet_id              = aws_subnet.private-2b.id
+  associate_public_ip_address = true
+  user_data = file("user_data/user_data.tpl")
+      tags = {
+      Name = "app-server-2"
+    }
+}
