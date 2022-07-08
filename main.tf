e4bd8dec27877bf490e3e4f7b07872b422f8b8af
Sourav Kundu <sourav12kundu@gmail.com>
2022-07-08 12:54:44 -0500

    tightened up vpc, removed extra subnet, added internet gateway and updated route in route table

--- main.tf
+++ main.tf
@@ -1,5 +1,7 @@
 resource "aws_vpc" "this" {
-  cidr_block = "10.20.20.0/26"
+  cidr_block = "10.20.20.0/27"
+  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc#enable_dns_support
+  enable_dns_support = true
   tags = {
     "Name" = "Application-1"
   }
@@ -20,14 +22,6 @@ resource "aws_subnet" "private-2b" {
     "Name" = "Application-1-private-2b"
   }
 }
-resource "aws_subnet" "private-2c" {
-  vpc_id            = aws_vpc.this.id
-  cidr_block        = "10.20.20.32/28"
-  availability_zone = "us-east-2c"
-  tags = {
-    "Name" = "Application-1-private-2c"
-  }
-}
 resource "aws_route_table" "this-rt" {
   vpc_id = aws_vpc.this.id
   tags = {
@@ -42,7 +36,14 @@ resource "aws_route_table_association" "private-2b" {
   subnet_id      = aws_subnet.private-2b.id
   route_table_id = aws_route_table.this-rt.id
 }
-resource "aws_route_table_association" "private-2c" {
-  subnet_id      = aws_subnet.private-2c.id
-  route_table_id = aws_route_table.this-rt.id
+resource "aws_internet_gateway" "this-igw" {
+  vpc_id = aws_vpc.this.id
+  tags = {
+    "Name" = "Application-1-gateway"
+  }
+}
+resource "aws_route" "internet-route" {
+  destination_cidr_block = "0.0.0.0/0"
+  route_table_id         = aws_route_table.this-rt.id
+  gateway_id             = aws_internet_gateway.this-igw.id
 }
