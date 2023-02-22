444847d1ac7acdec0f19c8f1a08cdf290b6d63b6
Sourav Kundu <skundu.dev@gmail.com>
2023-02-22 04:36:38 -0600

    formated code

--- variables.tf
+++ variables.tf
@@ -9,12 +9,12 @@ variable "access_key" {
   description = "The access_key that belongs to the IAM user"
   type        = string
   sensitive   = true
-  default = ""
+  default     = ""
 }
 #Define IAM User Secret Key
 variable "secret_key" {
   description = "The secret_key that belongs to the IAM user"
   type        = string
   sensitive   = true
-  default = ""
+  default     = ""
 }
