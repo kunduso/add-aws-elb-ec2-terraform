e0420f19c216488e756a58d325693ce84c12162b
Sourav Kundu <skundu.dev@gmail.com>
2023-03-04 07:19:09 -0600

    commented out instance and load balancer to get the subnet consolidated

--- compute.tf
+++ compute.tf
@@ -32,37 +32,37 @@ data "aws_ami" "amazon_ami" {
   most_recent = true
   owners      = ["amazon"]
 }
-resource "aws_instance" "app-server1" {
-  instance_type          = "t2.micro"
-  ami                    = data.aws_ami.amazon_ami.id
-  vpc_security_group_ids = [aws_security_group.http-sg.id]
-  subnet_id              = aws_subnet.private-2a.id
-  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance#associate_public_ip_address
-  associate_public_ip_address = true
-  tags = {
-    Name = "app-server-1"
-  }
-  user_data = file("user_data/user_data.tpl")
-}
-resource "aws_instance" "app-server2" {
-  instance_type               = "t2.micro"
-  ami                         = data.aws_ami.amazon_ami.id
-  vpc_security_group_ids      = [aws_security_group.http-sg.id]
-  subnet_id                   = aws_subnet.private-2b.id
-  associate_public_ip_address = true
-  user_data                   = file("user_data/user_data.tpl")
-  tags = {
-    Name = "app-server-2"
-  }
-}
-resource "aws_instance" "app-server3" {
-  instance_type               = "t2.micro"
-  ami                         = data.aws_ami.amazon_ami.id
-  vpc_security_group_ids      = [aws_security_group.http-sg.id]
-  subnet_id                   = aws_subnet.private-2c.id
-  associate_public_ip_address = true
-  user_data                   = file("user_data/user_data.tpl")
-  tags = {
-    Name = "app-server-3"
-  }
-}
+# resource "aws_instance" "app-server1" {
+#   instance_type          = "t2.micro"
+#   ami                    = data.aws_ami.amazon_ami.id
+#   vpc_security_group_ids = [aws_security_group.http-sg.id]
+#   subnet_id              = aws_subnet.private-2a.id
+#   # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance#associate_public_ip_address
+#   associate_public_ip_address = true
+#   tags = {
+#     Name = "app-server-1"
+#   }
+#   user_data = file("user_data/user_data.tpl")
+# }
+# resource "aws_instance" "app-server2" {
+#   instance_type               = "t2.micro"
+#   ami                         = data.aws_ami.amazon_ami.id
+#   vpc_security_group_ids      = [aws_security_group.http-sg.id]
+#   subnet_id                   = aws_subnet.private-2b.id
+#   associate_public_ip_address = true
+#   user_data                   = file("user_data/user_data.tpl")
+#   tags = {
+#     Name = "app-server-2"
+#   }
+# }
+# resource "aws_instance" "app-server3" {
+#   instance_type               = "t2.micro"
+#   ami                         = data.aws_ami.amazon_ami.id
+#   vpc_security_group_ids      = [aws_security_group.http-sg.id]
+#   subnet_id                   = aws_subnet.private-2c.id
+#   associate_public_ip_address = true
+#   user_data                   = file("user_data/user_data.tpl")
+#   tags = {
+#     Name = "app-server-3"
+#   }
+# }
