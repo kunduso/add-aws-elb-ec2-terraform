7db328239531d16a0cc6096b6dda679f24417a60
Sourav Kundu <skundu.dev@gmail.com>
2023-03-04 07:48:18 -0600

    removed commented code

--- main.tf
+++ main.tf
@@ -18,30 +18,6 @@ resource "aws_subnet" "private" {
     "Name" = "Application-1-private"
   }
 }
-# resource "aws_subnet" "private-2a" {
-#   vpc_id            = aws_vpc.this.id
-#   cidr_block        = "10.20.20.0/28"
-#   availability_zone = "us-east-2a"
-#   tags = {
-#     "Name" = "Application-1-private-2a"
-#   }
-# }
-# resource "aws_subnet" "private-2b" {
-#   vpc_id            = aws_vpc.this.id
-#   cidr_block        = "10.20.20.16/28"
-#   availability_zone = "us-east-2b"
-#   tags = {
-#     "Name" = "Application-1-private-2b"
-#   }
-# }
-# resource "aws_subnet" "private-2c" {
-#   vpc_id            = aws_vpc.this.id
-#   cidr_block        = "10.20.20.32/28"
-#   availability_zone = "us-east-2c"
-#   tags = {
-#     "Name" = "Application-1-private-2c"
-#   }
-# }
 resource "aws_route_table" "this-rt" {
   vpc_id = aws_vpc.this.id
   tags = {
@@ -53,14 +29,6 @@ resource "aws_route_table_association" "private" {
   subnet_id      = element(aws_subnet.private.*.id, count.index)
   route_table_id = aws_route_table.this-rt.id
 }
-# resource "aws_route_table_association" "private-2b" {
-#   subnet_id      = aws_subnet.private-2b.id
-#   route_table_id = aws_route_table.this-rt.id
-# }
-# resource "aws_route_table_association" "private-2c" {
-#   subnet_id      = aws_subnet.private-2c.id
-#   route_table_id = aws_route_table.this-rt.id
-# }
 resource "aws_internet_gateway" "this-igw" {
   vpc_id = aws_vpc.this.id
   tags = {
