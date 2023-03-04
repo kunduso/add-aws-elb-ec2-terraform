cb1ee385d1aed9fe27cdc8a4122435628ab960a2
Sourav Kundu <skundu.dev@gmail.com>
2023-03-04 07:16:48 -0600

    consolidated code

--- main.tf
+++ main.tf
@@ -9,48 +9,58 @@ resource "aws_vpc" "this" {
     "Name" = "Application-1"
   }
 }
-resource "aws_subnet" "private-2a" {
+resource "aws_subnet" "private" {
+  count = length(var.subnet_cidr_private)
   vpc_id            = aws_vpc.this.id
-  cidr_block        = "10.20.20.0/28"
-  availability_zone = "us-east-2a"
+  cidr_block        = var.subnet_cidr_private[count.index]
+  availability_zone = var.availability_zone[count.index]
   tags = {
-    "Name" = "Application-1-private-2a"
+    "Name" = "Application-1-private"
   }
 }
-resource "aws_subnet" "private-2b" {
-  vpc_id            = aws_vpc.this.id
-  cidr_block        = "10.20.20.16/28"
-  availability_zone = "us-east-2b"
-  tags = {
-    "Name" = "Application-1-private-2b"
-  }
-}
-resource "aws_subnet" "private-2c" {
-  vpc_id            = aws_vpc.this.id
-  cidr_block        = "10.20.20.32/28"
-  availability_zone = "us-east-2c"
-  tags = {
-    "Name" = "Application-1-private-2c"
-  }
-}
+# resource "aws_subnet" "private-2a" {
+#   vpc_id            = aws_vpc.this.id
+#   cidr_block        = "10.20.20.0/28"
+#   availability_zone = "us-east-2a"
+#   tags = {
+#     "Name" = "Application-1-private-2a"
+#   }
+# }
+# resource "aws_subnet" "private-2b" {
+#   vpc_id            = aws_vpc.this.id
+#   cidr_block        = "10.20.20.16/28"
+#   availability_zone = "us-east-2b"
+#   tags = {
+#     "Name" = "Application-1-private-2b"
+#   }
+# }
+# resource "aws_subnet" "private-2c" {
+#   vpc_id            = aws_vpc.this.id
+#   cidr_block        = "10.20.20.32/28"
+#   availability_zone = "us-east-2c"
+#   tags = {
+#     "Name" = "Application-1-private-2c"
+#   }
+# }
 resource "aws_route_table" "this-rt" {
   vpc_id = aws_vpc.this.id
   tags = {
     "Name" = "Application-1-route-table"
   }
 }
-resource "aws_route_table_association" "private-2a" {
-  subnet_id      = aws_subnet.private-2a.id
+resource "aws_route_table_association" "private" {
+  count = length(var.subnet_cidr_private)
+  subnet_id      = element(aws_subnet.*.id, count.index)
   route_table_id = aws_route_table.this-rt.id
 }
-resource "aws_route_table_association" "private-2b" {
-  subnet_id      = aws_subnet.private-2b.id
-  route_table_id = aws_route_table.this-rt.id
-}
-resource "aws_route_table_association" "private-2c" {
-  subnet_id      = aws_subnet.private-2c.id
-  route_table_id = aws_route_table.this-rt.id
-}
+# resource "aws_route_table_association" "private-2b" {
+#   subnet_id      = aws_subnet.private-2b.id
+#   route_table_id = aws_route_table.this-rt.id
+# }
+# resource "aws_route_table_association" "private-2c" {
+#   subnet_id      = aws_subnet.private-2c.id
+#   route_table_id = aws_route_table.this-rt.id
+# }
 resource "aws_internet_gateway" "this-igw" {
   vpc_id = aws_vpc.this.id
   tags = {
