4ce438f1d04c71f413d761331640cdcea5d8a3bc
Sourav Kundu <skundu.dev@gmail.com>
2023-03-05 06:12:37 -0600

    tf fmt

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -18,7 +18,7 @@ resource "aws_lb_target_group" "front" {
 }
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/lb_target_group_attachment
 resource "aws_lb_target_group_attachment" "attach-app1" {
-  count = length(aws_instance.app-server)
+  count            = length(aws_instance.app-server)
   target_group_arn = aws_lb_target_group.front.arn
   target_id        = element(aws_instance.app-server.*.id, count.index)
   port             = 80
