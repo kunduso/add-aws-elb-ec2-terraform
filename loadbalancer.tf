a3ec8fe4ebdb6642f126d82408c5eeb40110c44b
Sourav Kundu <skundu.dev@gmail.com>
2023-07-25 17:18:09 -0500

    tightened security group

--- loadbalancer.tf
+++ loadbalancer.tf
@@ -39,7 +39,7 @@ resource "aws_lb" "front" {
   name               = "front"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.http-sg.id]
+  security_groups    = [aws_security_group.lb.id]
   subnets            = [for subnet in aws_subnet.public : subnet.id]
 
   enable_deletion_protection = false
---
+++ securitygroup.tf
@@ -0,0 +1,50 @@
+resource "aws_default_security_group" "default" {
+  vpc_id = aws_vpc.this.id
+  ingress {
+    protocol  = -1
+    self      = true
+    from_port = 0
+    to_port   = 0
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+resource "aws_security_group" "ec2_instance" {
+  name        = "IN-SG"
+  description = "Allow inbound and outbound traffic to EC2 instances from load balancer security group"
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.lb.id]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  vpc_id = aws_vpc.this.id
+}
+
+resource "aws_security_group" "lb" {
+  name        = "LB-SG"
+  description = "Allow inbound and outbound traffic to load balancer from the internet."
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  vpc_id = aws_vpc.this.id
+}
