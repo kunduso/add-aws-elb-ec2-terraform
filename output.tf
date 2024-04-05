ed11bfcc68c388e72f2ea4b9e162c3e0a27dc394
Sourav Kundu <skundu.dev@gmail.com>
2024-04-05 08:11:15 -0500

    added http to dns output

--- output.tf
+++ output.tf
@@ -1,6 +1,6 @@
 output "instance_app-server_public_dns" {
-  value = aws_instance.app-server.*.public_dns
+  value = "http://${aws_instance.app-server.*.public_dns}"
 }
 output "load_balancer_dns_name" {
-  value = aws_lb.front.dns_name
+  value = "http://${aws_lb.front.dns_name}"
 }
