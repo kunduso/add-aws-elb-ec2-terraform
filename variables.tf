7e41ca1ecf6d68cb23084b46ab86ce3e74089a1c
Sourav Kundu <skundu.dev@gmail.com>
2025-03-19 12:40:11 -0500

    variables for vpc module #35

--- variables.tf
+++ variables.tf
@@ -18,15 +18,17 @@ variable "secret_key" {
   sensitive   = true
   default     = ""
 }
-
+variable "name" {
+  description = "The name of the application."
+  type        = string
+  default     = "app-1"
+}
+variable "vpc_cidr" {
+  description = "the vpc cidr"
+  default     = "10.20.20.0/26"
+}
 variable "subnet_cidr_public" {
   description = "cidr blocks for the public subnets"
   default     = ["10.20.20.0/28", "10.20.20.16/28", "10.20.20.32/28"]
-  type        = list(any)
-}
-
-variable "availability_zone" {
-  description = "availability zones for the public subnets"
-  default     = ["us-east-2a", "us-east-2b", "us-east-2c"]
   type        = list(any)
 }
