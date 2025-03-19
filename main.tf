7380f3b477e38419c4a0c81c3669fe2694132e73
Sourav Kundu <skundu.dev@gmail.com>
2025-03-19 12:39:22 -0500

    use vpc module to with flow logs enabled. Fix for #35

--- main.tf
+++ main.tf
@@ -1,42 +1,20 @@
-# https://docs.aws.amazon.com/glue/latest/dg/set-up-vpc-dns.html
-resource "aws_vpc" "this" {
-  cidr_block = "10.20.20.0/26"
-  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc#enable_dns_support
-  enable_dns_support = true
-  # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc#enable_dns_hostnames
-  enable_dns_hostnames = true
-  tags = {
-    "Name" = "app-1"
-  }
+data "aws_availability_zones" "available" {
+  state = "available"
 }
-resource "aws_subnet" "public" {
-  count             = length(var.subnet_cidr_public)
-  vpc_id            = aws_vpc.this.id
-  cidr_block        = var.subnet_cidr_public[count.index]
-  availability_zone = var.availability_zone[count.index]
-  tags = {
-    "Name" = "app-1-public-${count.index + 1}"
-  }
-}
-resource "aws_route_table" "this-rt" {
-  vpc_id = aws_vpc.this.id
-  tags = {
-    "Name" = "app-1-route-table"
-  }
-}
-resource "aws_route_table_association" "public" {
-  count          = length(var.subnet_cidr_public)
-  subnet_id      = element(aws_subnet.public.*.id, count.index)
-  route_table_id = aws_route_table.this-rt.id
-}
-resource "aws_internet_gateway" "this-igw" {
-  vpc_id = aws_vpc.this.id
-  tags = {
-    "Name" = "app-1-gateway"
-  }
-}
-resource "aws_route" "internet-route" {
-  destination_cidr_block = "0.0.0.0/0"
-  route_table_id         = aws_route_table.this-rt.id
-  gateway_id             = aws_internet_gateway.this-igw.id
+#https://registry.terraform.io/modules/kunduso/vpc/aws/1.0.3
+module "vpc" {
+  # Alinging with Checkov rule CKV_TF_1 and using a Git URLs with a commit hash revision. 
+  # Details available in below link:
+  # https://docs.prismacloud.io/en/enterprise-edition/policy-reference/supply-chain-policies/terraform-policies/ensure-terraform-module-sources-use-git-url-with-commit-hash-revision
+  source = "git::https://github.com/kunduso/terraform-aws-vpc.git?ref=53480d4bee43d23c547e931672d5f896e6e559c3" # commit hash of version 1.0.3
+  # source                  = "kunduso/vpc/aws"
+  # version                 = "1.0.3"
+  region                  = var.region
+  enable_dns_hostnames    = true
+  enable_dns_support      = true
+  enable_flow_log         = true
+  enable_internet_gateway = true
+  vpc_name                = var.name
+  vpc_cidr                = var.vpc_cidr
+  subnet_cidr_public      = var.subnet_cidr_public
 }
