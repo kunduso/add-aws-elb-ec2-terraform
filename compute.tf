033fbdb5f4360da2d5d4e7f1f730351bfda74346
Sourav Kundu <skundu.dev@gmail.com>
2025-03-19 12:43:17 -0500

    fix for #38 instance metadata service version 1 is not used

--- compute.tf
+++ compute.tf
@@ -37,11 +37,17 @@ resource "aws_instance" "app-server" {
   instance_type          = "t2.micro"
   ami                    = data.aws_ami.amazon_ami.id
   vpc_security_group_ids = [aws_security_group.ec2_instance.id]
-  subnet_id              = element(aws_subnet.public.*.id, count.index)
+  subnet_id              = element(module.vpc.public_subnets.*.id, count.index)
   # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance#associate_public_ip_address
   associate_public_ip_address = true
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 1
+    instance_metadata_tags      = "enabled"
+  }
   tags = {
     Name = "app-server-${count.index + 1}"
   }
-  user_data = file("user_data/user_data.tpl")
+  user_data = filebase64("./user_data/user_data.tpl")
 }
